940e9c10c14ae996d66420fe2b5add4e78551f96
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-06-11 11:42:45 +0200

    Configure token Oauth Request API and get res create a local jon file and debug mode variable because of 100 monthly request limitation

--- api_idealista.py
+++ api_idealista.py
@@ -1,16 +1,55 @@
 import requests
-#TODO 1: load .env date
 from dotenv import load_dotenv
-import os
+import os, base64, json
 
 project_folder = os.path.expanduser('.')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
+# Oauth token part
 APIKEY = os.getenv("APIKEY")
 SECRET = os.getenv("SECRET")
+AUTH_URL = "https://api.idealista.com/oauth/token"
+base64_credential = str(base64.b64encode(f"{APIKEY}:{SECRET}".encode("utf-8")), "utf-8")
+auth_header = f"Basic {base64_credential}"
 
-#TODO 2: Create a class
-#TODO 2.1: Class has json has property
+END_POINT = "https://api.idealista.com/3.5/es/search"
 
-#To set in .env file when it's working well
-END_POINT = ""
+debug_mode = True
+
+def get_new_token():
+
+   header = {
+       "Authorization": auth_header,
+       "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+   param = {
+       "grant_type": "client_credentials"
+   }
+
+   token_response = requests.post(AUTH_URL, headers=header, params=param)
+   print(token_response.json())
+   return token_response.json()
+
+token = get_new_token()
+
+param = {
+    "operation": "rent",
+    "propertyType": "homes",
+    "center": "40.123,-3.242",
+    "distance": "10000"
+}
+api_call_headers = {'Authorization': f"Bearer {token['access_token']}"}
+# Try to limit request and work with local jsonfile (./request_answer.json)
+if debug_mode:
+    api_call_response = requests.post(END_POINT, headers=api_call_headers, params=param)
+
+print(api_call_response.json())
+print(api_call_response.text)
+
+# #TODO 2: Create a class
+# class flatAppManager:
+#
+#     def __init__(self):
+#         self.answer_dict = {}
+# #TODO 2.1: Class has json has property
